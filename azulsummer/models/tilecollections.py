47f4d6d57ff58ea3a2539ebf7d078b8f7bad2ab1
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-01-07 23:25:43 -0600

    Update TileCollection and TileCollection subclasses
    Changed the TileCollection and TileCollection subclasses to be np.ndarray subclasses.  These were originally objects with array.array properties.  To streamline adding the tile collections together we changed these to straight np.arrays.

--- azulsummer/models/tilecollections.py
+++ azulsummer/models/tilecollections.py
@@ -1,86 +1,85 @@
 from __future__ import annotations
 
-from abc import ABC
-from array import array
-from random import sample
-from typing import Sequence
+import numpy as np
 
 from azulsummer.components import TileColor
 
-# TODO:  Subclass array.array for the TileCollections to avoid ugly "TileCollection.tile_count" smell OR add index, iterator, slice, etc. methods to the TileCollection
-# TODO:  Add a to_file() and from_file() / to_bytes()? from_bytes()? methods? to store the arrays
-# TODO:  Move "Refill bag from tower" to be a separately logged action?
 
-
-class TileCollection(ABC):
+class TileCollection(np.ndarray):
     """Base class for Tile Collections"""
 
     N_TILE_TYPES = 6
 
-    def __init__(self, tile_counts: Sequence[int]):
-        if len(tile_counts) != self.N_TILE_TYPES:
-            raise ValueError(
-                f"tile_counts {tile_counts} value is invalid.  tile_counts must be len of {self.N_TILE_TYPES}."
-            )
-        self.tile_count = array("B", tile_counts)
+    def __new__(cls, arr, *args, **kwargs):
+        obj = np.asarray(arr, dtype="B").view(cls)
+        return obj
+
+    def __array_finalize__(self, obj):
+        pass
 
     def __repr__(self):
         color_count = {
-            TileColor(idx).name: tile_count
-            for idx, tile_count in enumerate(self.tile_count)
+            TileColor(idx).name: tile_count for idx, tile_count in enumerate(self)
         }
         return f"{self.__class__.__name__}({color_count})"
 
-
     def zero(self):
         """Zero out a tile collection"""
-        self.tile_count = [0 for _ in self.tile_count]
+        self.fill(0)
 
 
 class TileBag(TileCollection):
     """Source of tiles for loading the factory displays and supply spaces"""
 
-    def __init__(self):
-        initial_tiles = [22] * self.N_TILE_TYPES
-        super().__init__(tile_counts=initial_tiles)
-        self.tower = Tower()
+    INITIAL_TILE_COUNT = 22
+
+    def __new__(cls, *args, **kwargs):
+        initial_tiles = [cls.INITIAL_TILE_COUNT] * super().N_TILE_TYPES
+        obj = super().__new__(cls, arr=initial_tiles)
+        return obj
+
+    def __array_finalize__(self, obj):
+        pass
 
     def draw(self) -> int:
         """Draw a tile from the bag"""
-        if sum(self.tile_count) == 0:
-            self.refill_bag_from_tower()
-        idx = sample(range(self.N_TILE_TYPES), k=1, counts=self.tile_count)[0]
-        self.tile_count[idx] -= 1
-        return idx
-
-    def refill_bag_from_tower(self):
-        for i, tiles in enumerate(self.tower.tile_count):
-            self.tile_count[i] = tiles
-        self.tower.zero()
-
-    def add_tiles(self, color: int, n_tiles: int = 1):
-        self.tower.tile_count[color] += n_tiles
+        pass
+        # if sum(self.tile_count) == 0:
+        #     self.refill_bag_from_tower()
+        # idx = sample(range(self.N_TILE_TYPES), k=1, counts=self.tile_count)[0]
+        # self.tile_count[idx] -= 1
+        # return idx
 
 
 class Tower(TileCollection):
     """Holds played tiles"""
 
-    def __init__(self):
-        initial_tiles = [0] * self.N_TILE_TYPES
-        super().__init__(tile_counts=initial_tiles)
+    INITIAL_TILE_COUNT = 0
+
+    def __new__(cls, *args, **kwargs):
+        initial_tiles = [cls.INITIAL_TILE_COUNT] * super().N_TILE_TYPES
+        obj = super().__new__(cls, arr=initial_tiles)
+        return obj
+
+    def __array_finalize__(self, obj):
+        pass
 
 
 class FactoryDisplay(TileCollection):
     """Holds drawable tiles"""
 
-    def __init__(self):
-        initial_tiles = [0] * self.N_TILE_TYPES
-        super().__init__(tile_counts=initial_tiles)
+    INITIAL_TILE_COUNT = 0
 
-    def fill_factory_display(self, bag: TileBag):
-        """Fill the factory display with 4 tiles from the tile bag"""
-        for i in range(4):
-            self.tile_count[bag.draw()] += 1
+    def __new__(cls, *args, **kwargs):
+        initial_tiles = [cls.INITIAL_TILE_COUNT] * super().N_TILE_TYPES
+        obj = super().__new__(cls, arr=initial_tiles)
+        return obj
+
+    def __array_finalize__(self, obj):
+        pass
+
+    def fill_factory_display(self, bag):
+        pass
 
     @classmethod
     def from_bag(cls, bag: TileBag) -> FactoryDisplay:
@@ -95,13 +94,20 @@ class SupplySpace(TileCollection):
     statues, pillars, windows
     """
 
-    def __init__(self):
-        initial_tiles = [0] * self.N_TILE_TYPES
-        super().__init__(tile_counts=initial_tiles)
+    INITIAL_TILE_COUNT = 0
+
+    def __new__(cls, *args, **kwargs):
+        initial_tiles = [cls.INITIAL_TILE_COUNT] * super().N_TILE_TYPES
+        obj = super().__new__(cls, arr=initial_tiles)
+        return obj
+
+    def __array_finalize__(self, obj):
+        pass
 
     def fill_supply_space(self, bag):
-        for i in range(10):
-            self.tile_count[bag.draw()] += 1
+        pass
+        # for i in range(10):
+        #     self.tile_count[bag.draw()] += 1
 
     @classmethod
     def from_bag(cls, bag: TileBag) -> SupplySpace:
@@ -113,20 +119,26 @@ class SupplySpace(TileCollection):
 class PlayerReserve(TileCollection):
     """Tiles held by each player"""
 
-    def __init__(self):
-        initial_tiles = [0] * self.N_TILE_TYPES
-        super().__init__(tile_counts=initial_tiles)
+    INITIAL_TILE_COUNT = 0
 
-    def add_tile(self, tile):
-        self.tile_count[tile] += 1
+    def __new__(cls, *args, **kwargs):
+        initial_tiles = [cls.INITIAL_TILE_COUNT] * super().N_TILE_TYPES
+        obj = super().__new__(cls, arr=initial_tiles)
+        return obj
+
+    def __array_finalize__(self, obj):
+        pass
 
 
 class MiddleOfFactory(TileCollection):
     """Tiles holding un-drawn factory display tiles"""
 
-    def __init__(self):
-        initial_tiles = [0] * self.N_TILE_TYPES
-        super().__init__(tile_counts=initial_tiles)
+    INITIAL_TILE_COUNT = 0
 
-    def add_tile(self, tile):
-        self.tile_count[tile] += 1
+    def __new__(cls, *args, **kwargs):
+        initial_tiles = [cls.INITIAL_TILE_COUNT] * super().N_TILE_TYPES
+        obj = super().__new__(cls, arr=initial_tiles)
+        return obj
+
+    def __array_finalize__(self, obj):
+        pass
