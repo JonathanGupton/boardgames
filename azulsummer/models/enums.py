b8e64bbf699aa25b72e5cf66edcc0f61da7216ff
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-01-09 00:15:57 -0600

    Create enums file and add state and player actions
    Create enum file containing state and player actions.
    These enums break down individual 'tick' actions to be done as part of the game.
    Moved the TileColors enum to the enums file.

---
+++ azulsummer/models/enums.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from enum import Enum, IntEnum, unique, auto
+
+
+@unique
+class TileColors(IntEnum):
+    """Tile color indices"""
+
+    Orange = 0
+    Red = 1
+    Blue = 2
+    Yellow = 3
+    Green = 4
+    Purple = 5
+
+
+@unique
+class Phase(Enum):
+    """Enum representing each phase in a round"""
+
+    AcquireTile = auto()
+    PlayTiles = auto()
+    PrepareNextRound = auto()
+
+
+@unique
+class PlayerActions(Enum):
+    """Enum representing each possible player action"""
+
+    DrawFromSupply = auto()
+    DrawFromFactoryDisplay = auto()
+    DrawFromMiddle = auto()
+    PlaceTile = auto()
+    DiscardExcessTiles = auto()
+
+
+@unique
+class StateActions(Enum):
+    """Enum representing actions taken by the game state"""
+
+    AdvancePhase = auto()
+    AdvanceCurrentPlayerIndex = auto()
+    AdvanceTurn = auto()
+    AdvanceWildTileIndex = auto()
+    AssignStartPlayer = auto()
+    IncrementScore = auto()
+    LoadBagFromTower = auto()
+    LoadFactoryDisplayTiles = auto()
+    LoadSupplyTiles = auto()
+    LoadTowerTiles = auto()
+    LoadMiddleTiles = auto()
+    UnassignStartPlayer = auto()
+    ScoreEndOfGame = auto()
