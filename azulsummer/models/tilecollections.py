8ca656086bbb27b9432781607e6f24c75fa005d9
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-01-09 12:36:00 -0600

    Add draw method to FactoryDisplay and other changes
    Add draw method to FactoryDisplay to allow the removal of tiles from a factory display
    Add is_empty method to base TileCollection class
    Update TileCollection repr to only show available tiles
    Improve docstrings for various TileCollection subclasses

--- azulsummer/models/tilecollections.py
+++ azulsummer/models/tilecollections.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Optional
+
 import numpy as np
 
 from azulsummer.models.enums import TileColor
@@ -19,7 +21,9 @@ class TileCollection(np.ndarray):
 
     def __repr__(self):
         color_count = {
-            TileColor(idx).name: tile_count for idx, tile_count in enumerate(self)
+            TileColor(idx).name: tile_count
+            for idx, tile_count in enumerate(self)
+            if tile_count
         }
         return f"{self.__class__.__name__}({color_count})"
 
@@ -27,9 +31,14 @@ class TileCollection(np.ndarray):
         """Zero out a tile collection"""
         self.fill(0)
 
+    def is_empty(self) -> bool:
+        """Check if the tile collection is empty of tiles"""
+        return sum(self) == 0
+
 
 class TileTransition(TileCollection):
     """Generic collection to transfer tiles between collections"""
+
     INITIAL_TILE_COUNT = 0
 
     def __new__(cls, *args, **kwargs):
@@ -42,7 +51,10 @@ class TileTransition(TileCollection):
 
 
 class TileBag(TileCollection):
-    """Source of tiles for loading the factory displays and supply spaces"""
+    """
+    Source of tiles for loading the factory displays and supply spaces
+    Initializes with 22 tiles of each color (132 in total) per the game.
+    """
 
     INITIAL_TILE_COUNT = 22
 
@@ -60,7 +72,10 @@ class TileBag(TileCollection):
 
 
 class Tower(TileCollection):
-    """Holds played tiles"""
+    """
+    Stores played tiles until the tile bag is empty
+    Initializes without any tiles
+    """
 
     INITIAL_TILE_COUNT = 0
 
@@ -74,7 +89,7 @@ class Tower(TileCollection):
 
 
 class FactoryDisplay(TileCollection):
-    """Holds drawable tiles"""
+    """Holds drawable factory display tiles"""
 
     INITIAL_TILE_COUNT = 0
 
@@ -86,9 +101,41 @@ class FactoryDisplay(TileCollection):
     def __array_finalize__(self, obj):
         pass
 
+    def draw(self, color: TileColor, n_tiles: Optional[int] = None) -> TileTransition:
+        """
+        Draw the given tile type from the display.
+
+        :argument
+            color: TileColor - The color to be drawn
+            n_tiles: Optional[int] - The number of tiles to be drawn.  The
+            default value of None returns all tiles of that color.  When
+            specified only n_tiles will be returned of the given color.
+
+        :returns
+            TileTransition object with the count of tiles drawn of the given color
+
+        :exception
+            Value error when n_tiles drawn is greater than available tiles
+        """
+
+        tile_transition = TileTransition()
+        if n_tiles is None:
+            tile_transition[color] = self[color]
+            self[color] = 0
+        else:
+            if n_tiles <= self[color]:
+                self[color] -= n_tiles
+                tile_transition[color] += n_tiles
+            else:
+                raise ValueError(
+                    f"Attempted draw of {n_tiles} of {TileColor(color).name} is greater than the available {self[color]} tiles"
+                )
+        return tile_transition
+
 
 class SupplySpace(TileCollection):
-    """10 tile supply to draw from when draw positions are filled e.g.,
+    """
+    10 tile supply to draw from when draw positions are filled e.g.,
     statues, pillars, windows
     """
 
