26f9dac0be5d4714df57621c917251c8f634ebaa
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-03-08 05:37:34 -0600

    Add various views, quantity and draw methods
    Add _SUPPLY_MAX property
    Add get_bag_quantity method
    Add get_tower_quantity method
    Add get_supply_quantity method
    Rename get_xxxx_n_view to get_nth_xxxx_view methods
    Begin draw_from_bag method
    Add refill_bag_from_tower
    Begin fill_supply method
    Add tests and test skeletons for the above methods

--- azulsummer/models/tiles.py
+++ azulsummer/models/tiles.py
@@ -34,9 +34,12 @@ class Tiles:
     _SUPPLY_INDEX: int = 3
     _FACTORY_DISPLAY_INDEX: int = 4
 
-    # 7 rows of 6 are assigned for each player's board
-    # 1 row for each color and 1 row for the 'wild' color
+    # 7 rows of 6 are assigned for each player's board which is
+    #   1 row for each color and 1 row for the 'wild' color
     _PLAYER_BOARD_RANGE: int = 7
+
+    # Max number of tiles held in the Supply row
+    _SUPPLY_MAX: int = 10
 
     def __init__(self, n_players: int, seed: Optional[int] = None) -> None:
         # Check for a valid number of players
@@ -60,13 +63,13 @@ class Tiles:
                 n_players * Tiles._PLAYER_BOARD_RANGE
         )
 
-        n_rows = (
+        n_tile_rows = (
                 4  # bag, tower, table center, and supply rows
                 + self.n_factory_displays
                 + (self.n_players * Tiles._PLAYER_BOARD_RANGE)
                 + self.n_players  # player reserves
         )
-        self._tiles: np.array = np.zeros((n_rows, len(TileColor)), "B")
+        self._tiles: np.array = np.zeros((n_tile_rows, len(TileColor)), "B")
 
         # Create the initial distribution of 22 tiles * 6 tile colors
         self._tiles[0] += np.array([Tiles._TILE_COUNT] * len(TileColor), "B")
@@ -80,19 +83,31 @@ class Tiles:
 
     def get_bag_view(self) -> np.ndarray:
         """Get the distribution of tiles in the bag."""
-        return self._tiles[Tiles._BAG_INDEX]
+        return self._tiles[self._BAG_INDEX]
+
+    def get_bag_quantity(self) -> int:
+        """Get the total number of tiles in the bag"""
+        return self.get_bag_view().sum()
 
     def get_tower_view(self) -> np.ndarray:
         """Get the distribution of tiles in the tower."""
-        return self._tiles[Tiles._TOWER_INDEX]
+        return self._tiles[self._TOWER_INDEX]
+
+    def get_tower_quantity(self) -> int:
+        """Get the total number of tiles in the tower"""
+        return self.get_tower_view().sum()
 
     def get_table_center_view(self) -> np.ndarray:
         """Get the distribution of tiles in the center of the table."""
-        return self._tiles[Tiles._TABLE_CENTER_INDEX]
+        return self._tiles[self._TABLE_CENTER_INDEX]
 
     def get_supply_view(self) -> np.ndarray:
         """Get the distribution of tiles in the supply."""
-        return self._tiles[Tiles._SUPPLY_INDEX]
+        return self._tiles[self._SUPPLY_INDEX]
+
+    def get_supply_quantity(self) -> int:
+        """Get the total number of tiles held in supply."""
+        return self.get_supply_view().sum()
 
     def get_factory_displays_view(self) -> np.ndarray:
         """Get the distribution of tiles across all factory displays.
@@ -100,9 +115,9 @@ class Tiles:
         Returns:
              The tile distributions as a 2D numpy array.
         """
-        return self._tiles
+        pass
 
-    def get_factory_display_n_view(self, factory_display_n: int) -> np.ndarray:
+    def get_nth_factory_display_view(self, factory_display_n: int) -> np.ndarray:
         """Get the distribution of tiles for the given factory display.
 
         Args:
@@ -117,7 +132,7 @@ class Tiles:
         """Get the distribution of tiles across all players' boards."""
         pass
 
-    def get_player_board_n_view(self, player_n: int) -> np.ndarray:
+    def get_nth_player_board_view(self, player_n: int) -> np.ndarray:
         """Get the distribution of tiles for the given player board.
 
         Args:
@@ -132,14 +147,18 @@ class Tiles:
         """Get the distribution of tiles across all player reserves"""
         pass
 
-    def get_player_n_reserve_view(self, player_n: int) -> np.ndarray:
+    def get_nth_player_reserve_view(self, player_n: int) -> np.ndarray:
         """Get the distribution of tiles in reserve that are held by player n."""
         pass
 
     """ MOVE TILES AROUND """
 
     def move_tiles(
-            self, source: int, destination: int, tile: Union[TileColor, int], quantity: int
+            self,
+            source: int,
+            destination: int,
+            tile: Union[TileColor, int],
+            quantity: int = 1,
     ) -> None:
         """
         Move tiles between two tile locations.
@@ -173,14 +192,32 @@ class Tiles:
         Returns:
             None
         """
-        pass
+        if (self.get_bag_quantity() < n_tiles) and (
+                (self.get_bag_quantity() + self.get_tower_quantity()) >= n_tiles
+        ):
+            pass
+
+        # multivariate_hypergeometric draws tiles without replacement from a
+        # 1D array of tiles e.g. [22, 22, 22, 22, 22, 22]
+        # results in a distribution of drawn tiles in a 1x6 shape such as
+        # [1, 2, 1, 3, 1, 1].
+        delta = self.rng.multivariate_hypergeometric(
+            self._tiles[self._BAG_INDEX], n_tiles
+        ).astype("B")
+
+    def refill_bag_from_tower(self) -> None:
+        """Move all tiles from the Tower to the Bag."""
+        self._tiles[self._BAG_INDEX] += self._tiles[self._TOWER_INDEX]
+        self._tiles[self._TOWER_INDEX] *= 0
+        self.validate_tile()
 
     def fill_supply(self) -> None:
         """Load the supply space from the bag."""
-        pass
-
-    def refill_bag_from_tower(self):
-        pass
+        unfilled_supply = self._SUPPLY_MAX - self.get_supply_quantity()
+        n_tiles_to_fill = min(
+            unfilled_supply, self.get_bag_quantity() + self.get_tower_quantity()
+        )
+        self.draw_from_bag(n_tiles_to_fill, self._SUPPLY_INDEX)
 
     """ VALIDATION """
 
--- azulsummer/test/test_tiles.py
+++ azulsummer/test/test_tiles.py
@@ -1,8 +1,10 @@
 """Tests for the Tiles class"""
 
+import numpy as np
 import pytest
 
-from ..models.tiles import Tiles
+from azulsummer.models.enums import TileColor
+from azulsummer.models.tiles import VALID_TILE_DISTRIBUTION, Tiles
 
 
 @pytest.mark.parametrize("n_players,n_tiles", [(2, 132), (3, 132), (4, 132)])
@@ -30,16 +32,10 @@ def test_n_players_out_of_range_error(n_players):
         t = Tiles(n_players)
 
 
-def test_initial_supply_load():
-    pass
-
-
-def test_refill_bag_from_tower():
-    pass
-
-
 @pytest.mark.parametrize("n_players", [2, 3, 4])
 def test_validate_invalid_tile_count(n_players):
+    """Test that the validate_tile method raises an error when there are more
+    than 132 tiles on the _tile property"""
     t = Tiles(n_players)
     t._tiles[0][0] += 1
     with pytest.raises(ValueError):
@@ -48,8 +44,71 @@ def test_validate_invalid_tile_count(n_players):
 
 @pytest.mark.parametrize("n_players", [2, 3, 4])
 def test_validate_invalid_column_tile_count(n_players):
+    """Test that the validate_tile method raises an error when any _tile
+    column != 22 tiles."""
     t = Tiles(n_players)
     t._tiles[0][0] += 1
     t._tiles[0][1] -= 1
     with pytest.raises(ValueError):
         t.validate_tile()
+
+
+def test_tiles_equals_valid_tile_distribution():
+    t = Tiles(2)
+    assert np.array_equal(t._tiles.sum(axis=0), VALID_TILE_DISTRIBUTION)
+
+
+def test_move_tiles_with_integer_tile():
+    """Test moving tiles between two tile locations with int tile location"""
+    t = Tiles(2)
+    t.move_tiles(0, 1, 0, 22)
+    assert t._tiles[0][0] == 0
+    assert t._tiles[1][0] == 22
+
+
+def test_move_tiles_with_tilecolor_enum():
+    """Test moving tiles between two tile locations with IntEnum tile location"""
+    t = Tiles(2)
+    t.move_tiles(0, 1, TileColor.Orange, 22)
+    assert t._tiles[0][TileColor.Orange] == 0
+    assert t._tiles[1][TileColor.Orange] == 22
+
+
+def test_move_tiles_value_error():
+    """Test a move that results in an invalid tile value"""
+    t = Tiles(2)
+    with pytest.raises(ValueError):
+        t.move_tiles(0, 1, TileColor.Orange, -1)  # overflow results in 255
+
+
+@pytest.mark.parametrize("n_players", [2, 3, 4])
+def test_get_bag_quantity_is_132_at_start(n_players):
+    t = Tiles(n_players)
+    assert t.get_bag_quantity() == 132
+
+
+@pytest.mark.parametrize("n_players", [2, 3, 4])
+def test_get_tower_quantity_is_0_at_start(n_players):
+    t = Tiles(n_players)
+    assert t.get_tower_quantity() == 0
+
+
+@pytest.mark.parametrize("n_players", [2, 3, 4])
+def test_refill_bag_from_tower(n_players):
+    t = Tiles(n_players)
+    t.move_tiles(0, 1, 0, 22)
+    t.refill_bag_from_tower()
+    assert np.array_equal(t.get_bag_view(), np.array([22, 22, 22, 22, 22, 22]))
+    assert np.array_equal(t.get_tower_view(), np.array([0, 0, 0, 0, 0, 0]))
+
+
+def test_draw_from_bag():
+    pass
+
+
+def test_draw_from_bag_more_than_available_from_bag_but_enough_in_tower():
+    pass
+
+
+def test_draw_from_bag_more_than_available_in_bag_and_tower():
+    pass
