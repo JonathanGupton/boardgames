9637922fc09c6c31ad65a77b324dfee028db2899
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-03-23 05:21:22 -0500

    Initial actions commit for Work_on_actions_class branch

--- azulsummer/models/actions.py
+++ azulsummer/models/actions.py
@@ -1,14 +1,27 @@
-def play_tile_to_board(state, player: int, star: int, tile: int):
-    pass
+"""Module containing logic for generating actions."""
+
+from abc import ABC, abstractmethod
+
+from azulsummer.models.enums import PlayerActions
 
 
-def draw_factory_display_tiles(state, player, factory_display, color):
-    pass
+class Action(ABC):
+    """Base class for holding action information.
+
+    Action instances hold the arguments used by the apply_action function and
+    are also used to store previous actions.
+    """
+    __slots__ = ["player", "action_type", "values"]
+
+    def __init__(self, player: int, action_type: PlayerActions, values: tuple) -> None:
+        self.player = player
+        self.action_type = action_type
+        self.values = values
+
+    @abstractmethod
+    def __repr__(self):
+        pass
 
 
-def draw_middle_display_tile(state, player, color):
-    pass
-
-
-def draw_tile_from_supply(state, player, color):
+def generate_actions(state) -> list:
     pass
