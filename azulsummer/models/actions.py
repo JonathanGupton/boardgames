a1ef0e700bd6a71a2560defcfe6fdb5661909eb3
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-03-23 08:03:28 -0500

    Refactor values arg as ActionValue, add repr to Action class

--- azulsummer/models/actions.py
+++ azulsummer/models/actions.py
@@ -1,11 +1,10 @@
 """Module containing logic for generating actions."""
 
-from abc import ABC, abstractmethod
-
+from azulsummer.models.actionvalues import ActionValue
 from azulsummer.models.enums import PlayerActions
 
 
-class Action(ABC):
+class Action:
     """Base class for holding action information.
 
     Action instances hold the arguments used by the apply_action function and
@@ -13,14 +12,13 @@ class Action(ABC):
     """
     __slots__ = ["player", "action_type", "values"]
 
-    def __init__(self, player: int, action_type: PlayerActions, values: tuple) -> None:
+    def __init__(self, player: int, action_type: PlayerActions, values: ActionValue) -> None:
         self.player = player
         self.action_type = action_type
         self.values = values
 
-    @abstractmethod
-    def __repr__(self):
-        pass
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.player}, {self.action_type}, {self.values})"
 
 
 def generate_actions(state) -> list:
