a3060a176bdc0e378a530dc8d71f00792faa58a8
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-03-23 04:56:20 -0500

    Refactor enums to be snake_case

--- azulsummer/models/enums.py
+++ azulsummer/models/enums.py
@@ -49,40 +49,39 @@ class WildTiles(IntEnum):
 class Phase(IntEnum):
     """Enum representing each phase in a round"""
 
-    AcquireTile = 0
-    PlayTiles = 1
-    PrepareNextRound = 2
+    acquire_tile = 0
+    play_tiles = 1
+    prepare_next_round = 2
 
 
 @unique
 class PlayerActions(Enum):
     """Enum representing each possible player action"""
 
-    AcquireTiles = auto()
-    DrawFromFactoryDisplay = auto()
-    DrawFromSupply = auto()
-    DrawFromMiddle = auto()
-    PlaceTile = auto()
-    DiscardExcessTiles = auto()
-    Pass = auto()
+    draw_from_factory_display = auto()
+    draw_from_supply = auto()
+    draw_from_middle = auto()
+    place_tile = auto()
+    discard_excess_tiles = auto()
+    pass_turn = auto()
 
 
 @unique
 class StateActions(Enum):
     """Enum representing actions taken by the game state"""
 
-    AdvancePhase = auto()
-    AdvanceCurrentPlayerIndex = auto()
-    AdvanceRound = auto()
-    AdvanceTurn = auto()
-    AdvanceWildTileIndex = auto()
-    AssignStartPlayer = auto()
-    DeclareWinner = auto()
-    EndOfGameScoring = auto()
-    IncrementScore = auto()
-    LoadBagFromTower = auto()
-    LoadTilesToCenter = auto()
-    LoadTilesToFactoryDisplay = auto()
-    LoadTilesToSupply = auto()
-    LoadTilesToTower = auto()
-    UnassignStartPlayer = auto()
+    advance_phase = auto()
+    advance_current_player_index = auto()
+    advance_round = auto()
+    advance_turn = auto()
+    advance_wild_tile_index = auto()
+    assign_start_player = auto()
+    declare_winner = auto()
+    end_of_game_scoring = auto()
+    increment_score = auto()
+    load_bag_from_tower = auto()
+    load_tiles_to_center = auto()
+    load_tiles_to_factory_display = auto()
+    load_tiles_to_supply = auto()
+    load_tiles_to_tower = auto()
+    unassign_start_player = auto()
