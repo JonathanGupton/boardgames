5a49637b8e70823c54092f03bcc98b40b281625e
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-07-03 09:51:49 -0500

    Change game_id from str to UUID

--- azulsummer/models/action.py
+++ azulsummer/models/action.py
@@ -1,4 +1,7 @@
 """Module containing the Action interface classes"""
+
+import uuid
+
 from dataclasses import dataclass
 
 
@@ -8,114 +11,120 @@ class Action:
 
 @dataclass
 class AdvancePhase(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class AdvanceCurrentPlayerIndex(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class AdvanceRound(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class AdvanceTurn(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class AdvanceWildTileIndex(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class AssignStartPlayer(Action):
-    game_id: str
-    player: str
+    game_id: uuid.UUID
+    player: uuid.UUID
 
 
 @dataclass
 class CreateGame(Action):
-    players: list[str]
+    pass
+
+
+@dataclass
+class AssignPlayer(Action):
+    player: uuid.UUID
 
 
 @dataclass
 class ScoreGame(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class IncrementPlayerScore(Action):
-    game_id: str
-    player: str
-    score: int
+    game_id: uuid.UUID
+    player: uuid.UUID
+    delta: int
 
 
 @dataclass
 class DecrementPlayerScore(Action):
-    game_id: str
-    player: str
-    score: int
+    game_id: uuid.UUID
+    player: uuid.UUID
+    delta: int
 
 
 @dataclass
 class LoadBagFromTower(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class LoadTilesToCenter(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class LoadTilesToFactoryDisplay(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class LoadTilesToSupply(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class LoadTilesToTower(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class UnAssignStartPlayer(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class DrawFromFactoryDisplay(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class DrawFromSupply(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class DrawFromMiddle(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class PlayTileToPlayerBoard(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class DiscardExcessTiles(Action):
-    game_id: str
+    game_id: uuid.UUID
 
 
 @dataclass
 class PassTurn(Action):
-    pass
+    game_id: uuid.UUID
+    player: uuid.UUID
