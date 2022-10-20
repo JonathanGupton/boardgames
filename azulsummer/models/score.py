962e61e5b12f9cb7adf68084f3a24992f942cba1
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-10-20 08:22:45 -0500

    Move COMPLETE_STAR_VALUE and COVER_ALL_VALUE to models.score module

--- azulsummer/models/score.py
+++ azulsummer/models/score.py
@@ -1,5 +1,29 @@
 import numpy as np
 
+from models.enums import StarColor
+
+# TODO:  Are these value objects really in the right place?
+# The point value for completing a star at the end of the game
+COMPLETE_STAR_VALUE = {
+    StarColor.Wild: 12,
+    StarColor.Red: 14,
+    StarColor.Blue: 15,
+    StarColor.Yellow: 16,
+    StarColor.Orange: 17,
+    StarColor.Green: 18,
+    StarColor.Purple: 20,
+}
+
+# The point value gained for covering all star pieces of the given cost,
+# e.g., covering the 1's on all stars provides 4 points
+COVER_ALL_VALUE = {
+    1: 4,
+    2: 8,
+    3: 12,
+    4: 16,
+}
+
+
 
 class Score(np.ndarray):
     """Base class for player scores
