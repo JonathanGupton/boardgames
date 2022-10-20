ce0770693222dccf1104fd7e832a123ed47730a0
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-10-20 16:48:01 -0500

    Add make_state() and new() method to Game class

--- azulsummer/models/game.py
+++ azulsummer/models/game.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
+from collections import deque
 from typing import Sequence, Optional
 from uuid import uuid4
 
+from azulsummer.models.state import State
 from azulsummer.players.player import Player
 
 
 class Game:
-    def __init__(self, players: Sequence[Player], seed: Optional[int] = None) -> None:
+    def __init__(
+        self, game_id: str, players: Sequence[Optional[Player]], seed: Optional[int]
+    ) -> None:
         """Initiate a game
 
         Args:
@@ -15,16 +19,26 @@ class Game:
                 the starting player.
             seed:  Optional int for the random seed
         """
-        self.id = str(uuid4())
+        self.game_id = game_id
         self.players = players
         self.seed = seed
         self.random = None
+        self.action_history = []
+        self.events = deque()
 
+        # state must be created with Game.make_state() after assigning players
+        # to the game
+        self.state = None
 
-# Ply - One turn taken by one of the players
+    @classmethod
+    def new(
+        cls, players: Optional[Sequence[Player]] = None, seed: Optional[int] = None
+    ):
+        """Instantiate a game without player information"""
+        players = players if players else []
+        seed = seed if seed else None
+        return cls(str(uuid4()), players, seed)
 
-"""
-Create the players
-Create a game 
-
-"""
+    def make_state(self):
+        """Create the initial game state for the game"""
+        self.state = State.new(len(self.players))
