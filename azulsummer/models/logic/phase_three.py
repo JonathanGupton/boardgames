4ff355ecea2b58064c02c31ba14faef23b4b7165
JonathanGupton <Jonathan.Gupton@gmail.com>
2022-10-20 16:44:56 -0500

    Map out phase three functionality

---
+++ azulsummer/models/logic/phase_three.py
@@ -0,0 +1,16 @@
+"""Module containing the logic for Phase Three of an Azul Summer Pavilion game"""
+
+
+def begin_phase_three(action):
+    # Advance the WildTile counter by 1
+    # queue up next phase
+    pass
+
+
+def advance_wild_tile():
+    pass
+
+
+def check_next_phase():
+    """Confirm if the game advances to phase 1 or ends to scoring"""
+    pass
